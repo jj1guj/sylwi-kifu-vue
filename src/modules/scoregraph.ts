1c70d50df1b6e5db4e793b9b4948d54ddbb32712
jj1guj <jj1guj@jj1guj.net>
2026-08-21 09:42:25 +0900

    余分な空白を削除

--- src/modules/scoregraph.ts
+++ src/modules/scoregraph.ts
@@ -1512,7 +1512,7 @@ const doWrite = (p: Partial<SvgScoreGraphProp>): VNode =>
         textSideW: "",
         caption: "",
         capLink: "",
-        plyCallback: () => {},  
+        plyCallback: () => {},
       },
       p
     )
